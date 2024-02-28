2b101009ec1be0d2d4998d4c8e51c5e5f1b3e67e
Tom Daniel <0xtomdaniel@gmail.com>
2024-02-28 13:00:32 -0500

    set up github actions

--- scripts/staging.bash
+++ scripts/staging.bash
@@ -3,4 +3,4 @@
 scriptdir="$(dirname "$0")"
 cd "$scriptdir"
 
-docker compose -p ember-agents-staging -f ../docker/docker-compose.staging.yml up
+docker compose -p ember-agents-staging -f ../docker/docker-compose.staging.yml up -d
