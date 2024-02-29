81478c5b5d1d94254c93c2da4b6c70e65433a43d
Tom Daniel <0xtomdaniel@gmail.com>
2024-02-29 14:18:26 -0500

    test non-zero exit from python

--- src/ember_agents/__init__.py
+++ src/ember_agents/__init__.py
@@ -1,6 +1,13 @@
+import sys
+import time
 from dotenv import load_dotenv
 
 print("\nInitializing ember_agents package...")
+
+# DEBUG
+time.sleep(2)
+print("\nNon-zero exit...")
+sys.exit(1)
 
 load_dotenv()
 
