9e27aca48d3571032e7551853245f71397f22879
Tom Daniel <0xtomdaniel@gmail.com>
2024-03-01 16:56:42 -0500

    stop docker project to invoke changes

--- scripts/staging.bash
+++ scripts/staging.bash
@@ -4,4 +4,5 @@ scriptdir="$(dirname "$0")"
 cd "$scriptdir"
 
 #
+docker compose -p ember-core_staging -f ../docker/docker-compose.staging.yml stop
 docker compose -p ember-core_staging -f ../docker/docker-compose.staging.yml up -d
