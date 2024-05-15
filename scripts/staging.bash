42759df7c2a44c2a840015933f7468c20af1842a
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-05-14 22:01:37 -0500

    Simplify devops for ember engine side.

--- scripts/staging.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#scriptdir="$(dirname "$0")"
-#cd "$scriptdir"
-
-docker compose -p ember-core_staging -f docker/docker-compose.staging.yml stop
-docker compose -p ember-core_staging -f docker/docker-compose.staging.yml up -d
