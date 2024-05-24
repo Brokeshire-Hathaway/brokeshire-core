088f220cd5c9a2347a6525e106eabb15d53b5d6e
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-05-24 17:49:59 -0500

    Add sign url interaction.

--- ember_agents/send_token/send.py
+++ ember_agents/send_token/send.py
@@ -39,15 +39,7 @@ class TxRequest(BaseModel):
 
 
 class TxPreview(BaseModel):
-    recipient: str
-    amount: str
-    fees: str
-    total: str
-    transaction_uuid: str
-
-
-class ExecuteTxBody(BaseModel):
-    transaction_uuid: str
+    url: str
 
 
 class Transaction(BaseModel):
@@ -57,28 +49,7 @@ class Transaction(BaseModel):
     token: str
 
 
-class UserReceipt(BaseModel):
-    success: bool
-    recipient: str
-    amount: str
-    fees: str | None
-    total: str | None
-    transaction_block: str
-
-
 OAI_CONFIG_LIST = [{"model": "gpt-4-1106-preview", "api_key": SETTINGS.openai_api_key}]
-
-
-async def prepare_transaction(tx_request: TxRequest):
-    url = f"{SETTINGS.transaction_service_url}/transactions/prepare"
-    async with httpx.AsyncClient(http2=True, timeout=65) as client:
-        response = await client.post(url, json=tx_request.model_dump())
-
-    try:
-        response_json = response.json()
-        return TxPreview.model_validate(response_json)
-    except ValidationError as err:
-        raise ValueError(response_json.get("message", "Failed sending token")) from err
 
 
 # Create a temporary file
@@ -148,14 +119,7 @@ class SendTokenGroupChat(GroupChat):
                     case "transaction_coordinator":
                         next_speaker = self.agent_by_name("user")
                     case "user":
-                        if self.last_speaker.name == "transaction_coordinator":
-                            next_speaker = self.agent_by_name("executor")
-                        else:
-                            next_speaker = self.last_speaker
-                    case "executor":
-                        next_speaker = self.agent_by_name("user")
-                    case "confirmation_specialist":
-                        next_speaker = self.agent_by_name("user")
+                        next_speaker = self.last_speaker
                     case "technician":
                         next_speaker = self.last_speaker
 
@@ -340,51 +304,11 @@ NEXT: interpreter"""
 # TODO: Add new agent for showing tx preview, determining if any changes are needed, and
 #       confirming if the user will proceed or cancel. Broker might be able to handle this.
 
-# PROBLEM:  The executor expects a reply after sending the preview and signature link to the user. However, the user must sign using the URL, not by replying to the executor.
-#
-#           - Maybe the prepare transaction function should send the preview and signature link to the user and only return with a success or failure result.
-#           - Another option might be to have the human input method to check if the last message from the executor. If so, then it will reply either from the user or signing result. Whichever comes first.
-
 # TODO: Skip signature link for now and just have the user reply with proceed or cancel.
-executor_system_message = """
-You are an executor responsible for determining whether the user will proceed or cancel their transaction request. If they proceed, you must reply with \"NEXT: confirmation_specialist\" to pass the request to the confirmation specialist. If they cancel, you must reply with \"Transaction canceled\nTERMINATE\" to end the conversation.
-"""
-
-
-# NOTE: Still working out this signature link prompt.
-"""
-You are an executor responsible for showing the user a preview of their transaction request along with a signature link.
-After sending the user the preview, you must use the "get_transaction_update" function to check the status of the transaction.
-The user should open the signature link in their web browser and sign the transaction to proceed.
-You will monitor the status of the transaction and update the user as needed.
-If the transaction fails or the user decides to cancel the request, you must use \"TERMINATE\" to pass along the reason.
-"""
-
-# 1. intent -> request (user -> interpreter)
-# 2. request -> validate (interpreter -> validator)
-# 3. a. valid -> execute (validator -> executor)
-#    b. I. invalid -> ask for missing information until satisfied (validator -> broker)
-#       II. new intent -> request (broker -> interpreter)
 
 
 technician_system_message = """
 You are a technician responsible for executing tools and returning the results to the requester.
-"""
-
-# DEPRECATED PROMPTS
-#
-validator_system_message = """
-You are a validator responsible for determining if a request is valid.
-A request is only valid if it contains a Recipient Address, Amount, and Token Address.
-If the request is valid, you must use \"NEXT: executor\" to pass the request to the executor.
-If the request is invalid, list the missing information.
-Be brief and clear.
-You must append \"NEXT: broker\" to pass along your message.
-"""
-
-"""executor_system_message =
-You are an executor responsible for showing the user a preview of their transaction request and asking them to confirm if they will proceed or cancel.
-You must use the "a_prepare_transaction" function to prepare the transaction and get the preview. After getting a confirmation from the user, you must use the "get_transaction_result" function to get the outcome of the transaction and pass it along to the user. You must append \"TERMINATE\" to pass along the result and end the conversation.
 """
 
 
@@ -418,11 +342,6 @@ class SendTokenAgentTeam(AgentTeam):
             llm_config=llm_config,
         )
 
-        """@technician.register_for_execution()
-        @executor.register_for_llm(
-            description="Prepare a transaction for the user to review and sign."
-        )"""
-
         async def a_prepare_transaction(
             recipient: ConversableAgent, messages, sender, config
         ):
@@ -435,15 +354,26 @@ class SendTokenAgentTeam(AgentTeam):
             )
             return await self._prepare_transaction(tx_request)"""
 
-            self._send_activity_update("Preparing transaction preview...")
+            self._send_activity_update("Setting up send transaction...")
 
             try:
                 if self._transaction_request is None:
                     msg = "Transaction request not found"
                     raise ValueError(msg)
-                self._transaction_preview = await prepare_transaction(
-                    self._transaction_request
-                )
+
+                url = f"{SETTINGS.transaction_service_url}/send/prepare"
+                async with httpx.AsyncClient(http2=True, timeout=65) as client:
+                    response = await client.post(
+                        url, json=self._transaction_request.model_dump()
+                    )
+
+                try:
+                    response_json = response.json()
+                    self._transaction_preview = TxPreview.model_validate(response_json)
+                except ValidationError as err:
+                    raise ValueError(
+                        response_json.get("message", "Failed sending token")
+                    ) from err
             except Exception as e:
                 error_message = str(e) if str(e) else str(type(e))
                 return (
@@ -455,13 +385,11 @@ TERMINATE""",
                 )
 
             # tx_details = self._transaction_preview.tx_details
-            response_message = f"""You are about to send 💸 {self._transaction_preview.amount} to {self._transaction_preview.recipient}.
 
-**💸 Subtotal ・** {self._transaction_preview.amount}
-**⛽️ Fees Estimation ・** {self._transaction_preview.fees}
-**🔢 Total ・** {self._transaction_preview.total}
+            response_message = f"""Your send transaction has been set up! 💸.
 
-Would you like to proceed?"""
+Finish your transaction [here]({self._transaction_preview.url})
+TERMINATE"""
 
             return True, {
                 "content": response_message,
@@ -479,113 +407,11 @@ Would you like to proceed?"""
             ConversableAgent, a_prepare_transaction, 1
         )
 
-        # TODO: Can be converted to Metis agent.
-        executor = AssistantAgent(
-            "executor", system_message=executor_system_message, llm_config=llm_config
-        )
-
-        """# TODO: This is a temporary method that will be replaced by the get_transaction_result method.
-        @technician.register_for_execution()
-        @executor.register_for_llm(description="Execute a transaction.")"""
-
-        async def a_execute_transaction(
-            recipient: ConversableAgent, messages, sender, config
-        ):
-            # self._send_activity_update("Executing transaction...")
-
-            async def repeating_task(seconds, task, *args):
-                while True:
-                    await task(*args)
-                    await asyncio.sleep(seconds)
-
-            async def send_info_bite():
-                bite = get_random_info_bite()
-                message = f"""Executing transaction...
-
-> **・ Fun Fact ・**
->🍬 {bite}"""
-                self._send_activity_update(message)
-
-            seconds = 8
-            timer_task = asyncio.create_task(repeating_task(seconds, send_info_bite))
-
-            try:
-                if self._transaction_preview is None:
-                    msg = "Transaction request not found"
-                    raise ValueError(msg)
-
-                url = f"{SETTINGS.transaction_service_url}/transactions/send"
-                body = ExecuteTxBody(
-                    transaction_uuid=self._transaction_preview.transaction_uuid
-                )
-                # HEADERS = {"Content-Type": "application/json"}
-                async with httpx.AsyncClient(http2=True, timeout=65) as client:
-                    response = await client.post(url, json=body.model_dump())
-
-                data = response.json()
-                try:
-                    user_receipt = UserReceipt.model_validate(data)
-                except ValidationError as err:
-                    msg = f"Failed sending token: {data.get('message', 'Unknown exception')}"
-                    raise Exception(msg) from err
-
-                if not user_receipt.success:
-                    msg = "Failed sending token"
-                    raise Exception(msg)
-            except Exception as e:
-                error_message = str(e) if str(e) else str(type(e))
-                return (
-                    True,
-                    f"""Your transaction failed. 😔
-
-Details: {error_message}
-TERMINATE""",
-                )
-            finally:
-                timer_task.cancel()
-            fees_message = (
-                (f"\n**⛽️ Fees ・** {user_receipt.fees}\n")
-                if user_receipt.fees is not None
-                else ""
-            )
-            total_message = (
-                f"\n**🔢 Total ・** {user_receipt.total}\n"
-                if user_receipt.total is not None
-                else ""
-            )
-            response_message = f"""Your transaction was successful! 🎉
-
-**👤 Recipient ・** {user_receipt.recipient}
-**💸 Amount Sent ・** {user_receipt.amount} {fees_message}{total_message}
-_[🔗 View on Blockchain]({user_receipt.transaction_block})_
-TERMINATE"""
-
-            return True, {
-                "content": response_message,
-                "name": "confirmation_specialist",
-                "role": "assistant",
-            }
-
-        # (Spock)
-        confirmation_specialist = AssistantAgent(
-            "confirmation_specialist",
-            None,
-            llm_config=llm_config,
-        )
-        confirmation_specialist.register_reply(
-            ConversableAgent, a_execute_transaction, 1
-        )
-
         technician = AssistantAgent(
             "technician",
             system_message=technician_system_message,
             llm_config=llm_config,
         )
-
-        """@technician.register_for_execution()
-        @executor.register_for_llm(description="Get the results of a transaction.")
-        async def a_get_transaction_result(tx_id: str):
-            return await self._get_transaction_result(tx_id)"""
 
         groupchat = SendTokenGroupChat(
             agents=[
@@ -594,8 +420,6 @@ TERMINATE"""
                 validator,
                 broker,
                 transaction_coordinator,
-                executor,
-                confirmation_specialist,
                 technician,
             ],
             messages=[],
