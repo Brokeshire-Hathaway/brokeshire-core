f05036d7f7e38af120a1507240a9f414cca760ac
Tom Daniel <0xtomdaniel@gmail.com>
2024-06-29 17:13:50 -0400

    Cleaned things up

--- ember_agents/agent_router/intent_classifier.py
+++ ember_agents/agent_router/intent_classifier.py
@@ -1,9 +1,6 @@
-import sys
 from math import exp
-from pprint import pprint
 from typing import Literal
 
-import numpy as np
 from fireworks.client import Fireworks
 from pydantic import BaseModel
 
@@ -21,6 +18,15 @@ INTENT = Literal[
     "out_of_scope",
 ]
 
+
+class ClassifiedIntent(BaseModel):
+    intent: INTENT
+    linear_probability: float
+
+
+client = Fireworks(api_key=SETTINGS.fireworks_api_key)
+
+
 descriptions: dict[INTENT, str] = {
     "capabilities_query": "Reply to any questions about me. Help the user understand what this assistant can do, it's features and functionalies, and how to use it. Its name is Ember.",
     "crypto_price_query": "Get the price of a cryptocurrency or token",
@@ -33,30 +39,12 @@ descriptions: dict[INTENT, str] = {
     "out_of_scope": "Does not fit any of the other intents",
 }
 
-
-class ClassifiedIntent(BaseModel):
-    intent: INTENT
-    linear_probability: float
-
-
-client = Fireworks(api_key=SETTINGS.fireworks_api_key)
-
-# possible_intents = "\n".join(f'"{i}: {item}"' for i, item in enumerate(INTENT.__args__))
-
 intent_strings = [f"{key} ({value})" for key, value in descriptions.items()]
-
 possible_intents = "\n".join(intent_strings)
-
 intent_grammar = f"""
 root ::= intent
 intent ::= "{'" | "'.join(INTENT.__args__)}"
 """
-
-"""
-root ::= intent
-intent ::= {' | '.join(f'"{i}"' for i in range(len(INTENT.__args__)))}
-"""
-
 SYSTEM_PROMPT = f"""You are a natural language understanding expert that classifies a user utterance into an intent. Select one of following possible intents. Your response must be a single name.
 
 # Possible Intents
@@ -78,16 +66,7 @@ async def classify_intent(utterance: str) -> ClassifiedIntent:
     Classify the intent of an utterance
     """
 
-    print(f"utterance: {utterance}")
-    # print(f"possible_intents: {possible_intents}")
-    # print(f"intent_grammar: {intent_grammar}")
-
-    # sys.exit()
-
     prompt = get_prompt(utterance)
-    # print(prompt)
-
-    # number_of_intents = len(INTENT.__args__)
     response = client.completion.create(
         model="accounts/fireworks/models/llama-v3-70b-instruct",
         response_format={"type": "grammar", "grammar": intent_grammar},
@@ -96,24 +75,8 @@ async def classify_intent(utterance: str) -> ClassifiedIntent:
         logprobs=True,
         top_logprobs=3,
     )
-
-    # print(response.choices[0].logprobs.content[0].top_logprobs)
-
-    """for content in response.choices[0].logprobs.content:
-        # print("Content:", content)
-        for token in content.top_logprobs:
-            print("Token:", token.token)
-            # print("Log prob:", token.logprob)
-            print("Linear prob:", np.round(exp(token.logprob) * 100, 2), "%")
-        break"""
-
     intent = response.choices[0].text
-
-    # index = int(response.choices[0].text)
-    # intent = INTENT.__args__[index]
     token_logprob = response.choices[0].logprobs.content[0].top_logprobs[0]
     linear_probability = exp(token_logprob.logprob)
 
-    """for content in response.choices[0].logprobs.content:
-        pprint(content.top_logprobs)"""
     return ClassifiedIntent(intent=intent, linear_probability=linear_probability)
--- tests/agent_router/test_intent_classifier.py
+++ tests/agent_router/test_intent_classifier.py
@@ -4,60 +4,79 @@ from ember_agents.agent_router.intent_classifier import INTENT, classify_intent
 
 
 @pytest.mark.parametrize(
-    "utterance, expected_intent",
+    "utterance, expected_intent, expected_is_confident",
     [
         (
             "@EmberAIBot  What is the cryptocurrency with the highest trading volume today and why? 🤔",
-            "market_news",
+            "market_news_query",
+            True,
         ),
-        ("how is ember ai different from unibot or bananabot?", "capabilities"),
+        (
+            "how is ember ai different from unibot or bananabot?",
+            "capabilities_query",
+            True,
+        ),
         (
             "Is your wallet readily available, and how do you see the idea of telegram wallets",
-            "capabilities",
+            "capabilities_query",
+            True,
         ),
         (
             "Is your wallet readily available",
-            "capabilities",
+            "capabilities_query",
+            True,
         ),
         (
             "how do you see the idea of telegram wallets",
-            "capabilities",
+            "explanation_query",
+            False,
         ),
-        ("jeo boden", "definition"),
-        ("tell me about arweave", "explanation"),
-        ("how much is doge?", "crypto_price"),
-        ("poocoin convert price point news outlet", "market_news"),
-        ("change a tire", "out_of_scope"),
-        ("the metal is soft because grass is green", "out_of_scope"),
-        ("What can you do?", "capabilities"),
-        ("wif price", "crypto_price"),
-        ("cost of link", "crypto_price"),
-        ("swap op", "swap_crypto"),
-        ("give eth to friend", "transfer_crypto"),
-        ("change sol for arb", "swap_crypto"),
-        ("give me bitcoin", "transfer_crypto"),
-        ("my friend wants some eth", "transfer_crypto"),
-        ("What is an L3?", "explanation"),
-        ("should I short bitcoin", "advice"),
-        ("best trading strategy", "advice"),
-        ("What are market news ?", "market_news"),
-        ("how to use", "capabilities"),
+        (
+            "jeo boden",
+            "unclear",
+            True,
+        ),
+        ("tell me about arweave", "explanation_query", True),
+        ("how much is doge?", "crypto_price_query", True),
+        ("poocoin convert price point news outlet", "unclear", True),
+        ("change a tire", "out_of_scope", True),
+        ("the metal is soft because grass is green", "unclear", True),
+        ("What can you do?", "capabilities_query", True),
+        ("wif price", "crypto_price_query", True),
+        ("cost of link", "crypto_price_query", True),
+        ("swap op", "swap_crypto_action", True),
+        ("give eth to friend", "transfer_crypto_action", True),
+        ("change sol for arb", "swap_crypto_action", True),
+        ("give me bitcoin", "crypto_price_query", True),
+        ("my friend wants some eth", "transfer_crypto_action", True),
+        ("What is an L3?", "explanation_query", True),
+        ("should I short bitcoin", "advice_query", True),
+        ("best trading strategy", "advice_query", True),
+        ("What are market news ?", "market_news_query", True),
+        ("how to use", "capabilities_query", True),
         (
             "Long or short on bitcoin? For a short term trade? What do you think has better chances of success?",
-            "advice",
+            "advice_query",
+            True,
         ),
-        ("features", "capabilities"),
+        ("features", "capabilities_query", True),
         (
             "hi @EmberAIBot What are the 3 key signs that investing in a token might be a bad idea?",
-            "advice",
+            "advice_query",
+            True,
         ),
-        ("anything exciting happen this week", "market_news"),
-        ("@EmberAIBot What's up What will be the next X100 crypto gem?", "advice"),
+        ("anything exciting happen this week", "market_news_query", True),
+        (
+            "@EmberAIBot What's up What will be the next X100 crypto gem?",
+            "advice_query",
+            True,
+        ),
     ],
 )
 # @pytest.mark.skip
-async def test_classify_intent(utterance: str, expected_intent: INTENT):
-    print("\n\ntesting...\n\n---\n\n")
-    intent = await classify_intent(utterance)
-    print(f"classification: {intent}")
-    # assert await classify_intent(utterance) == expected_intent
+async def test_classify_intent(
+    utterance: str, expected_intent: INTENT, expected_is_confident: bool
+):
+    classified_intent = await classify_intent(utterance)
+    assert classified_intent.intent == expected_intent
+    assert (classified_intent.linear_probability >= 0.8) is expected_is_confident
